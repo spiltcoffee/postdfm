4d4911898e5a3cdb3b6205a569f6e66fd6696c2d
SpiltCoffee <spiltcoffee@users.noreply.github.com>
2019-03-05 09:09:02 +1100

    WIP(build, dfm2ast): changed format of test summary; updated yarn.lock
    affects: @postdfm/dfm2ast

--- packages/@postdfm/dfm2ast/jest.config.js
+++ packages/@postdfm/dfm2ast/jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  "reporters": ["default", "jest-junit"]
+  "reporters": [
+    "default",
+    [
+      "jest-junit",
+      {
+        suiteNameTemplate: "{filename}",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}"
+      }
+    ]
+  ],
 };
