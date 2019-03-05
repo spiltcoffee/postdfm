c4679e0999e0ed948d874fa236e2784fdb45f5b0
SpiltCoffee <spiltcoffee@users.noreply.github.com>
2019-03-05 19:19:33 +1100

    chore: added commitlint, husky, semantic-release
    ISSUES CLOSED: #8

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    "@commitlint/config-conventional",
+    "@commitlint/config-lerna-scopes"
+  ],
+  rules: {
+    "type-enum": [2, "always", [
+      "feat",
+      "fix",
+      "docs",
+      "style",
+      "refactor",
+      "perf",
+      "test",
+      "chore",
+      "revert",
+      "wip"
+    ]]
+  }
+}
