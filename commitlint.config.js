7cb24bb3316def6d6b198d7e0fd4e0fc9d54285a
spiltcoffee <spiltcoffee@users.noreply.github.com>
2019-03-11 11:26:14 +1100

    chore(lint): changed commit linting to use conventional changelog
    lets stop being fancy
    
    fix #62

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,3 @@
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional",
-    "@commitlint/config-lerna-scopes"
-  ],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "fix",
-        "docs",
-        "style",
-        "refactor",
-        "perf",
-        "test",
-        "chore",
-        "revert",
-        "wip"
-      ]
-    ]
-  }
+  extends: ["@commitlint/config-conventional"]
 };
