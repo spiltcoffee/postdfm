948a57916be82d7e577fb922f593b215286ab40b
spiltcoffee <spiltcoffee@users.noreply.github.com>
2019-08-17 23:46:40 +1000

    fix: export parse and stringify as defaults as well
    fix #135

--- packages/@postdfm/ast2dfm/src/index.ts
+++ packages/@postdfm/ast2dfm/src/index.ts
@@ -1,7 +1,10 @@
 import { Root } from "@postdfm/ast";
 import Stringifier from "./stringifier";
 
-export function stringify(ast: Root): string {
+function stringify(ast: Root): string {
   const stringifier = new Stringifier();
   return stringifier.stringify(ast);
 }
+
+export default stringify;
+export { stringify };
