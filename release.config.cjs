883cbd1d05d3784a5dcd150ae059a60b33e8b8fb
spiltcoffee <spiltcoffee@users.noreply.github.com>
2023-07-15 16:41:27 +1000

    build: fix order of dependency updates for release

--- release.config.cjs
+++ release.config.cjs
@@ -1,14 +1,40 @@
 const path = require("path");
 const { spawnSync } = require("child_process");
 
-const pkgs = spawnSync("yarn", ["workspaces", "list", "--json"], {
-  shell: true,
-  encoding: "utf-8",
-})
+// ordered by dependency first, name second
+const pkgs = [
+  { name: "@postdfm/ast", pkgRoot: "packages/@postdfm/ast" },
+  { name: "@postdfm/ast2dfm", pkgRoot: "packages/@postdfm/ast2dfm" },
+  { name: "@postdfm/dfm2ast", pkgRoot: "packages/@postdfm/dfm2ast" },
+  { name: "@postdfm/plugin", pkgRoot: "packages/@postdfm/plugin" },
+  { name: "@postdfm/transform", pkgRoot: "packages/@postdfm/transform" },
+  { name: "postdfm", pkgRoot: "packages/postdfm" },
+];
+
+const workspaces = spawnSync(
+  "yarn",
+  ["workspaces", "list", "--json", "--no-private"],
+  { shell: true, encoding: "utf-8" },
+)
   .stdout.split(/[\r\n]+/)
   .filter(Boolean)
   .map(JSON.parse)
-  .filter(({ location }) => location !== ".");
+  .map(({ name, location }) => ({ name, pkgRoot: location }));
+
+function isPkgEqual(a) {
+  return (b) => a.name === b.name && a.pkgRoot === b.pkgRoot;
+}
+
+if (
+  !(
+    workspaces.every((workspace) => pkgs.some(isPkgEqual(workspace))) &&
+    pkgs.every((pkg) => workspaces.some(isPkgEqual(pkg)))
+  )
+) {
+  throw new Error(
+    "Mismatch between public workspaces and packages in release configuration!",
+  );
+}
 
 const tarballDir = path.resolve(__dirname, "dist");
 
@@ -18,13 +44,9 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["./release/dependency.cjs", { pkgs }],
-    ...pkgs.map(({ location: pkgRoot }) => [
-      "@semantic-release/npm",
-      {
-        pkgRoot,
-        tarballDir,
-      },
+    ...pkgs.flatMap((pkg) => [
+      ["./release/dependency.cjs", { pkg, pkgs }],
+      ["@semantic-release/npm", { pkgRoot: pkg.pkgRoot, tarballDir }],
     ]),
     "./release/yarnlock.cjs",
     [
